ecc6f09213862df8122edf369877d32c745552b9
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2026-07-01 09:57:21 +0700

    [refactor] Update compute_metrics function and add pytest configuration

--- metrics.py
+++ metrics.py
@@ -1,23 +1,20 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import precision_score, recall_score, f1_score
 
-def compute_metrics(y_true, t_pred) -> dict:
+
+def compute_metrics(y_true, y_pred) -> dict:
     """
     Computes evaluation metrics for binary classification.
 
     Args:
         y_true (list or array): True labels (0 or 1).
-        t_pred (list or array): Predicted labels (0 or 1).
+        y_pred (list or array): Predicted labels (0 or 1).
 
     Returns:
         dict: A dictionary containing precision, recall, and F1 score.
     """
-
-    precision = precision_score(y_true, t_pred)
-    recall = recall_score(y_true, t_pred)
-    f1 = f1_score(y_true, t_pred)
-
+    kwargs = {"zero_division": 0}
     return {
-        'precision': precision,
-        'recall': recall,
-        'f1': f1
+        "precision": float(precision_score(y_true, y_pred, **kwargs)),
+        "recall": float(recall_score(y_true, y_pred, **kwargs)),
+        "f1": float(f1_score(y_true, y_pred, **kwargs)),
     }
